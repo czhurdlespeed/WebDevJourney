013c102a061f60344365a04d879af583dfb39921
Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-09-30 20:43:25 -0400

    Improved Pet Component; Focused on making more, smaller components
    Props in react are read-only, so we can't change them. We can only
    change the state of the component.
    
    React Dev Tools allows you to inspect your components, their states, and
    props.

--- IntroReactV8/AdoptMe/src/SearchParams.jsx
+++ IntroReactV8/AdoptMe/src/SearchParams.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
     const [location, setLocation] = useState("");
@@ -69,14 +70,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
-            {pets.map(pet => (
-                <Pet 
-                    name={pet.name}
-                    animal={pet.animal}
-                    breed={pet.breed}
-                    key={pet.id}
-                />
-            ))}
+            <Results pets={pets} />
         </div>
     );
 };
